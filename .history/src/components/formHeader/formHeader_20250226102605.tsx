63dfb43a30d679890129a6ca4c2bfe1c16bab8bf
Fateme Yosefi <86098248+fateme81yosefi@users.noreply.github.com>
2025-02-26 11:29:31 +0330

    header and tabs add

---
+++ .history/src/components/formHeader/formHeader_20250226102605.tsx
@@ -0,0 +1,28 @@
+'use client'; 
+
+
+interface PageHeaderProps {
+  title: string;
+  onEdit: () => void;
+  onDelete: () => void;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({ title, onEdit, onDelete }) => {
+  return (
+    <div className="flex items-center justify-between w-full border-b pb-2">
+      <h1 className="text-xl font-semibold">{title}</h1>
+      <div className="flex items-center gap-4 w-full max-w-xs">
+        <div className="flex-1 border-t border-gray-300"></div>
+        <button   onClick={onEdit}>
+          ویرایش
+        </button>
+        <Button variant="destructive" size="sm" onClick={onDelete}>
+          <Trash className="w-4 h-4 mr-2" />
+          حذف
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PageHeader;
---
+++ .history/src/components/formHeader/formHeader_20250226103137.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+
+interface PageHeaderProps {
+    title: string;
+    onEdit: () => void;
+    onDelete: () => void;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({ title, onEdit, onDelete }) => {
+    return (
+        <div className="flex items-center justify-between w-full  pb-2">
+            <h1 className="text-xl font-semibold">{title}</h1>
+            <div className="flex items-center gap-4 w-full ">
+                <div className="flex-1 w-full border-t border-gray-300"></div>
+                <button onClick={onEdit}>
+                    ویرایش
+                </button>
+                <button onClick={onDelete}>
+                    حذف
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default PageHeader;
