6c333ec4965468c153f2ef4dd9729c7a1687d7a8
Soroush <166734554+Soroushq@users.noreply.github.com>
2025-02-28 21:10:31 +0330

    classes page
    some inputs added

--- src/app/classification/[boxName]/classes/page.tsx
+++ src/app/classification/[boxName]/classes/page.tsx
@@ -1,8 +1,17 @@
+import PageHeader from "@/components/formHeader/formHeader";
+
 export default function DefineServicesPage() {
     return (
       <div>
-        <h1 className="text-2xl font-bold mb-4">نمایش کلاس ها</h1>
-        <p>This is the content for نمایش کلاس ها. You can add forms, tables, or other components here.</p>
+        <div className="flex items-center justify-between w-full  pb-2">
+            <h2 className="text-2xl font-bold mb-4 text-nowrap">  کلاس های باشگاه</h2>
+            <div className=" w-full ">
+            <div className="flex-1 w-full mb-4 border-t border-gray-300 "></div>
+            </div>
+        </div>
+        
+        
+                <p>contents to be!</p>
       </div>
     );
   }
